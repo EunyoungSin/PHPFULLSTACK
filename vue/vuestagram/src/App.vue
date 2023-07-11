661c18ce4a2b7cb1105abee0fa7b81bd70a70e92
EunyoungSin <ey5742@gmail.com>
2023-07-11 16:53:02 +0900

    0711

--- vue/vuestagram/src/App.vue
+++ vue/vuestagram/src/App.vue
@@ -1,20 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <!-- 헤더 -->
+  <div class="header">
+    <ul>
+      <li v-if="$store.state.tabFlg !== 0" @click="$store.commit('changeTabFlg', 0); $store.commit('clearState');" class="header-button header-button-left">취소</li>
+      <li>
+        <img class="logo" alt="Vue logo" src="./assets/logo3.png">
+      </li>
+      <li v-if="$store.state.tabFlg == 1" @click="$store.commit('changeTabFlg', 2)" class="header-button header-button-right">다음</li>
+    </ul>
+  </div>
+
+  <!-- 컨텐츠 -->
+  <ConteinerComponent></ConteinerComponent>
+  <!-- <PostComponent></PostComponent> -->
+
+  <button v-if="$store.state.addBtnFlg && $store.state.tabFlg == 0" @click="$store.dispatch('getMoreList');" id="moreBtn">더보기</button>
+
+  <!-- 푸터 -->
+  <div class="footer">
+    <div class="footer-button-store">
+      <label for="file" class="label-store">+</label>
+      <input @change="updateImg" accept="image/*" type="file" id="file" class="input-file">
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ConteinerComponent from './components/ConteinerComponent.vue';
+// import PostComponent from './components/PostComponent.vue';
 
 export default {
   name: 'App',
+  created() {
+    this.$store.dispatch('getMainList');
+  },
+  methods: {
+    updateImg(e) {
+      let file = e.target.files;
+      let imgUrl = URL.createObjectURL(file[0]);
+      this.$store.commit('changeImgUrl',imgUrl);
+      this.$store.commit('changeTabFlg',1);
+      e.target.value = ''; // 이벤트 타겟 밸류 초기화
+    }
+  },
   components: {
-    HelloWorld
-  }
+    ConteinerComponent,
+    // PostComponent,
+}
 }
 </script>
 
 <style>
+@import url('./assets/css/common.css');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- vue/vuestagram/src/store.js
+++ vue/vuestagram/src/store.js
@@ -1,1 +1,80 @@
 // 데이터 저장소
+import { createStore } from 'vuex'
+import axios from 'axios'
+
+const store = createStore({
+    state() {
+        return {
+            boardData: [], // 게시글 데이터
+            lastId: '', // 가장 마지막에 로드된 게시물의 ID
+            addBtnFlg: true, // 더보기 버튼 표시용 flg
+            tabFlg: 0, // 탭UI flg(0:메인, 1:필터, 2:작성)
+            imgUrl: '', // 이미지 url
+            imgFilter: '',
+        }
+    },
+    mutations: { // 함수
+        // 초기 데이터 셋팅용. 배열타입
+        createBoardData(state, data) {
+            state.boardData = data;
+            this.commit('changeLastId', data[data.length - 1].id);
+        },
+        // 더보기 데이터 셋팅용. 객체타입
+        addBoardData(state, data) {
+            state.boardData.push(data);
+            this.commit('changeLastId', data.id);
+        },
+        // lastId 변경
+        changeLastId(state,id) {
+            state.lastId = id;
+        },
+        // 탭UI flg 변경
+        changeTabFlg(state,num) {
+            state.tabFlg = num;
+        },
+        // 이미지 url 변경
+        changeImgUrl(state, imgUrl) {
+            state.imgUrl = imgUrl;
+        },
+        // 이미지 필터 변경
+        changeFilter(state, imgFilter) {
+            state.filter = imgFilter;
+        },
+        // 초기화
+        clearState(state) {
+            state.filter = '';
+            state.imgUrl = '';
+        }
+    },
+    actions: {
+        getMainList(context) {
+            axios.get('http://192.168.0.66/api/boards')
+            .then(res => {
+                // console.log(res.data);
+                context.commit('createBoardData',res.data);
+            })
+            .catch ( err => {
+                console.log(err);
+            })
+        },
+        getMoreList(context) {
+            axios.get('http://192.168.0.66/api/boards/' + context.state.lastId)
+            .then(res => {
+                if(res.data){
+                    context.commit('addBoardData',res.data);
+                }
+                else{
+                    context.state.addBtnFlg = false; // 한군데에서만 쓰니까 mutations에 함수 만들지않고 생략
+                    alert('더 이상 표시할 내용이 없습니다.');
+                    // document.getElementById('moreBtn').style.display = 'none';
+                }
+            })
+            .catch ( err => {
+                console.log(err);
+            })
+        }
+    },
+    
+})
+
+export default store

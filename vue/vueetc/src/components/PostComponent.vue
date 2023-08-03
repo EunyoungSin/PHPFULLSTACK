9edb44fc0b36cb2b4cc915d3240ceedd73e1e908
EunyoungSin <ey5742@gmail.com>
2023-08-03 17:59:57 +0900

    0803

--- vue/vueetc/src/components/PostComponent.vue
+++ vue/vueetc/src/components/PostComponent.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
         <h1>포스트</h1>  
-        {{ test }}
+        {{ propsTest.value }}
     </div>
 </template>
 <script>
+import { ref, toRefs } from 'vue';
+
 export default {
     name:'PostComponent',
     props: {
-        test: Number,
+        propsTest: Number,
+    },
+    setup(props) {
+        let data = ref([]);
+        let { propsTest } = toRefs(props);
+        
+        console.log(propsTest.value);
+        return { data };
     }
+
 }
 </script>
 <style>
